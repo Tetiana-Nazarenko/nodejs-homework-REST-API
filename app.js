2c4dcbef359497c066e84ac12d051c5d131c9656
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-11-09 22:12:55 +0200

    add connect and schema

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+// const express = require('express');
+// const logger = require('morgan');
+// const cors = require('cors');
 
-const contactsRouter = require('./routes/api/contacts');
+// const contactsRouter = require('./routes/api/contacts');
 
-const app = express();
+// const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
+// app.use(logger(formatsLogger));
+// app.use(cors());
+// app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+// app.use('/api/contacts', contactsRouter);
 
-app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
+// app.use((req, res) => {
+//     res.status(404).json({ message: 'Not found' });
+// });
+
+// app.use((err, req, res, next) => {
+//     res.status(500).json({ message: err.message });
+// });
+
+// module.exports = app;
+
+const mongoose = require('mongoose');
+const DB_HOST =
+    'mongodb+srv://nnn27725:bOlXabXbzr849Voo@cluster0.p6nkayh.mongodb.net/';
+
+mongoose
+    .connect(DB_HOST)
+    .then(() => console.log('Database connection successful'))
+    .catch(error => {
+        console.log(error.message);
+        process.exit(1);
+    });
+
+const Schema = mongoose.Schema;
+
+const contacts = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Set name for contact'],
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    },
 });
-
-app.use((err, req, res, next) => {
-    res.status(500).json({ message: err.message });
-});
-
-module.exports = app;
